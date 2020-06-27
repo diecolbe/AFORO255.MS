4424df110fa3933e85941a1d9d910f8429b69d74
diego fernando colorado <diego_sis84@hotmail.com>
2020-06-27 18:02:13 -0500

    [AFORO255.TEST]- Se crea version inicial del proyecto.

---
+++ AFORO255.MS.TEST.Transaction/Handlers/DependencyInyectionHandlercs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AFORO255.MS.TEST.Transaction.Handlers
+{
+    public static class DependencyInyectionHandlercs
+    {
+
+    }
+}
